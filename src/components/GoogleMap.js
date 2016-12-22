cb93aacf23ac1956e95f4d5a35a7ddd53a96b3a0
Lasse S. Haslev <lasse@haslev.no>
2016-12-22 17:57:39 +0100

    Moved Google maps logic into own mixins file

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,4 +1,4 @@
-import GoogleMapsLoader from 'google-maps';
+import BaseGoogleMap from './BaseGoogleMap';
 export default {
 
     template: `
@@ -7,50 +7,5 @@ export default {
         </div>
     `,
 
-    props: {
-        'api-key': {
-            type: String,
-            default: null,
-        },
-    },
-
-    data() {
-        return {
-            google:null,
-            map:null,
-            lat:61.0638268,
-            lng:6.5696338,
-            zoom:8,
-        };
-    },
-    mounted() {
-        GoogleMapsLoader.KEY = this.apiKey;
-        this.loadMap();
-    },
-
-    methods: {
-        loadMap() {
-            var self = this;
-            GoogleMapsLoader.load(function(google) {
-                window.google = google;
-                self.$set( self, 'google', google );
-                var map = new google.maps.Map(self.$el, {
-                    center: {lat: self.lat, lng: self.lng },
-                    zoom: self.zoom
-                });
-
-                self.$emit( 'ready', map );
-
-                self.$set( self, 'map', map );
-
-                google.maps.event.addDomListener( window, 'resize', function() {
-                    self.setMapCenter();
-                } );
-
-            });
-        },
-        setMapCenter() {
-            this.map.setCenter( { lat: this.lat, lng: this.lng } );
-        },
-    }
+    mixins: [ BaseGoogleMap ],
 }
